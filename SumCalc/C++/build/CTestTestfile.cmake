672b9b01ef7a7f684f7738141b95d25767172a71
Adobe-Android <tngamerboy@gmail.com>
2019-05-24 23:55:34 -0500

    Remove extra folders and build files

--- SumCalc/C++/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dbrown/workspace/Polyglot/lang-benchmark/SumCalc/C++
-# Build directory: /home/dbrown/workspace/Polyglot/lang-benchmark/SumCalc/C++/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
