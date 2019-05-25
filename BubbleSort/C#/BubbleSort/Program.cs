07a7121c76ca5425a1395445b502ab026df36aed
David Brown <tngamerboy@gmail.com>
2019-05-25 00:39:11 -0500

    Add starter C# BubbleSort implementation

--- BubbleSort/C#/BubbleSort/Program.cs
+++ BubbleSort/C#/BubbleSort/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace BubbleSort
 {
@@ -6,7 +6,29 @@ namespace BubbleSort
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] intArr = { 38, 48, 89, 71, 87, 48, 97, 84, 39, 55};
+            BubbleSort(intArr);
+            PrintArray(intArr);
+        }
+
+        static void BubbleSort(int[] intArr)
+        {
+            int n = intArr.Length - 1;
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n - i; j++)
+                    if (intArr[j] > intArr[j + 1])
+                    {
+                        int temp = intArr[j];
+                        intArr[j] = intArr[j + 1];
+                        intArr[j + 1] = temp;
+                    }
+        }
+
+        static void PrintArray(int[] intArr)
+        {
+            int n = intArr.Length;
+            for (int i = 0; i < n; ++i)
+                Console.Write(intArr[i] + " ");
         }
     }
 }
