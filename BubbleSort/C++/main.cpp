0baee89739b2f20f7686f43b2657e9c2dc0a00e6
David Brown <tngamerboy@gmail.com>
2019-05-25 10:03:06 -0500

    Added C++ bubble sort implementation

--- BubbleSort/C++/main.cpp
+++ BubbleSort/C++/main.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
+#include <iterator>
+
+void BubbleSort(int intArr[], int n);
+void PrintArray(int intArr[], int n);
 
 int main()
 {
-    std::cout << "Coming Soon...";
+    int intArr[]{38, 48, 89, 71, 87, 48, 97, 84, 39, 55};
+    int n{std::size(intArr) - 1};
+    BubbleSort(intArr, n);
+    PrintArray(intArr, n);
     return 0;
+}
+
+void BubbleSort(int intArr[], int n)
+{
+    for (int i{0}; i < n; i++)
+        for (int j{0}; j < n - i; j++)
+            if (intArr[j] > intArr[j + 1])
+            {
+                int temp{intArr[j]};
+                intArr[j] = intArr[j + 1];
+                intArr[j + 1] = temp;
+            }
+}
+
+void PrintArray(int intArr[], int n)
+{
+    for (int i{0}; i < n + 1; ++i)
+        std::cout << intArr[i] << " ";
 }
