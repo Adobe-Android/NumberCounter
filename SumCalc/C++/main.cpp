9ee95db9cc0ead8551c8822a88bec5ff5d6a2e28
David Brown <tngamerboy@gmail.com>
2019-05-25 09:51:37 -0500

    Fixed weird spacing and modernized some code
    Fixed weird spacing and a converted remaining variables to use uniform initialization

--- SumCalc/C++/main.cpp
+++ SumCalc/C++/main.cpp
@@ -6,11 +6,11 @@ using std::vector;
 
 int main()
 {
-	long int_sum = 0; vector<int> sum_vector;
+	long int_sum{0}; vector<int> sum_vector;
 
-	for (int i = 1; i < 100'000'000; i++)
+	for (int i{1}; i < 100'000'000; i++)
 		if (i % 3 == 0 || i % 5 == 0)
-			sum_vector.push_back({ i });
+			sum_vector.push_back({i});
 
 	for (const int& j : sum_vector)
 		int_sum += j;
